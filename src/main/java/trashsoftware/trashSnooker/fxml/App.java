b2d0bbefb350f4d5f0ab417b0688b43eaf29041a
Bohan Zhang <bokhaam.zhang@mail.utoronto.ca>
2023-06-12 21:00:25 +1000

    Update App.java

--- src/main/java/trashsoftware/trashSnooker/fxml/App.java
+++ src/main/java/trashsoftware/trashSnooker/fxml/App.java
@@ -19,8 +19,8 @@ import java.util.ResourceBundle;
 @SuppressWarnings("all")
 public class App extends Application {
     
-    public static final String VERSION_NAME = "0.4.3";
-    public static final int VERSION_CODE = 36;
+    public static final String VERSION_NAME = "0.4.3.1";
+    public static final int VERSION_CODE = 37;
 
     private static final String CONFIG = "user" + File.separator + "config.cfg";
     public static final String CLASSIFIER = "win";
